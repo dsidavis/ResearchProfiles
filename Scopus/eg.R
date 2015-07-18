270afe686eb992fc2d2bc3565417fbcdae6e25a3
Duncan Temple Lang <duncan@r-project.org>
2015-07-18 15:38:33 -0700

    experimenting

--- Scopus/eg.R
+++ Scopus/eg.R
@@ -1,3 +1,24 @@
+library(RCurl)
+library(RJSONIO)
+
 doi = "10.1111/j.2041-210X.2012.00247.x"
 txt = getDoc(doi)
+
+
+
+# This doesn't work. It is from the federated search page, but the URL is incorrect
+#txt = getForm("http://api.elsevier.com/content/search/index:SCOPUS", query = "visualization", view = "COMPLETE",
+#               .opts = list(httpheader = c('X-ELS-APIKey' = getOption("ScopusKey"))))
+
+
+
+Scopus Search
+txt = getForm("http://api.elsevier.com/content/search/scopus", query = "visualization",  #view = "COMPLETE",
+               .opts = list(httpheader = c('X-ELS-APIKey' = getOption("ScopusKey"))))
+
+# count can be set more than 25?
+
+
+Affiliation search
+getForm("http://api.elsevier.com/documentation/AFFILIATIONSearchAPI.wadl", 
 
--- Scopus/scopus.R
+++ Scopus/scopus.R
@@ -17,3 +17,16 @@ function(q, key = getOption("ScopusKey", stop("need the scopus API key")), curl 
      #http://api.elsevier.com/content/search/scopus
     
 }
+
+scoAffiliation =
+function(query, ..., key = getOption("ScopusKey", stop("need the scopus API key")),  url = "http://api.elsevier.com/content/search/affiliation", curl = getCurlHandle())
+{
+   scopusQuery(q = query, ..., key = key, curl = curl, url = url)
+}
+
+scopusQuery =
+function(..., url, curl = getCurlHandle(), key = getOption("ScopusKey", stop("need the scopus API key")), .opts = list())
+{
+  .opts$httpheader = c('X-ELS-APIKey' = key)
+  getForm(url, .params = list(...), .opts = .opts, curl = curl)
+}
